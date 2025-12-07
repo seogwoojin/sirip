04ca87c65b487b087c44d5838939907131f71261
seogwoojin <server@uos.ac.kr>
2025-12-07 23:50:53 +0900

    feat: 최종

--- k6/k6_test.js
+++ k6/k6_test.js
@@ -5,7 +5,7 @@ const BASE_URL = __ENV.API_BASE_URL || 'http://localhost:8080';
 
 // 유저 20명
 const users = [];
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 200; i++) {
   users.push({
     email: `admin${i}@uos.ac.kr`,
     password: 'encoded_password',
@@ -31,8 +31,8 @@ export function setup() {
 }
 
 export const options = {
-  vus: 20,
-  iterations: 20,
+  vus: 200,
+  iterations: 200,
   thresholds: {
     http_req_failed: ['rate<0.05'],
     http_req_duration: ['p(95)<1000'],
@@ -44,7 +44,7 @@ export default function (data) {
   const token = data.tokens[__VU - 1];
   if (!token) return;
 
-  const res = http.post(`${BASE_URL}/api/events/9/coupons`, null, {
+  const res = http.post(`${BASE_URL}/api/events/13/coupons`, null, {
     headers: { Authorization: `Bearer ${token}` },
   });
 
